7c1f228c69ee44b1583f5a49cd9632bc6fbd89bd
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-11-16 23:15:59 -0300

    Finalizando primeira parte do exercicio 2.1.3

---
+++ estruturasCondicionais.js
@@ -0,0 +1,15 @@
+//primeira parte do exercicio
+if(caminho ===5 && corro === 2)
+{
+   console.log('passo o dia bem'); 
+} 
+
+if(levanto === 'cedo' || levanto < 8)
+{
+    console.log('saio tranquilo');
+} 
+
+if(!termino_trabalho || !tenho_compromisso)
+{
+    console.log('saio tarde');
+}
