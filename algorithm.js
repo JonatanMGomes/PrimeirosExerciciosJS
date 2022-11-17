3883897ad191f7517ba33eef9baa89ec3215cfe1
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-11-16 22:35:57 -0300

    Finalizando exercicios colocando em pratica, 2.1.1 e 2.2.2 após ter de refazer os repositorios.

---
+++ algorithm.js
@@ -0,0 +1,20 @@
+//pratica 2.1.1
+var soma = 5+5
+var subtracao = -1 - (-7)
+var divisao = 80/3
+var restoDiv = 80%3
+
+soma++
+soma++
+soma++
+soma++
+soma++
+
+soma--
+soma--
+soma--
+
+//pratica 2.2.2
+caminho_5km&&corro_2km, passo_dia_bem
+levanto_cedo||antes_8h, saio_tranquilo
+!termino_trabalho||!tenho_compromisso, saio_tarde
