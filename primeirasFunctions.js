1ab8ee217ecdc5e4f50a308f4ef320b75a269116
Jonatan M Gomes <jonatan.mim@hotmail.com>
2022-11-20 21:11:48 -0300

    finalizando exercicio 2.1.6

--- primeirasFunctions.js
+++ primeirasFunctions.js
@@ -14,3 +14,88 @@ function CheckUserInformation(userName, userPassWord){
 //3
 GetUserInformation()
 CheckUserInformation(userName, userPassWord);
+
+////////////////////////////
+//colocando em prática 2.1.6
+
+//1
+function confirmarDados(nome){
+    var nomeCompleto = nome + ' da Silva';
+    var idade = 20;
+    function getAnoNascimento(){
+        var ano = 2022 - idade;
+        return ano;
+    }
+
+    console.log('nome completo:', nomeCompleto);
+    console.log('ano de nascimento:', getAnoNascimento());
+    console.log('idade:', idade);
+}
+
+confirmarDados('Marcos');
+
+//2
+function confirmarDados(nome){
+    let nomeCompleto = nome + ' da Silva';
+    let geracao = 'millenium';
+    
+    function getAnoNascimento(){
+        const idade = 40;
+        const ano = 2022 - idade;
+        return ano;
+    }
+    let anoDeNascimento = getAnoNascimento();
+    if (anoDeNascimento <= 1995) {
+        geracao = 'geração x';
+    }
+
+    console.log('nome completo:', nomeCompleto);
+    console.log('ano de nascimento:', getAnoNascimento());
+    console.log('geração:', geracao);
+}
+
+confirmarDados('Marcos');
+
+//3
+try {
+    function confirmarDados(nome){
+        var nomeCompleto = nome + ' da Silva';
+        
+        function getAnoNascimento(){
+            var idade = 20;
+            var ano = 2022 - idade;
+            return ano;
+        }
+    
+        console.log('nome completo:', nomeCompleto);
+        console.log('ano de nascimento:', getAnoNascimento());
+        console.log('idade:', idade);
+    }
+    
+    confirmarDados('Marcos');
+} catch (error) {
+    console.log('Um erro foi encontrado e não foi possível concluir a operação.')
+}
+
+//4
+function confirmarDados(nome, idade){
+    var nomeCompleto = nome + ' da Silva';
+    var geracao = geracaoPertence();
+    
+    function getAnoNascimento(){
+        var ano = 2022 - idade;
+        return ano;
+    }
+
+    function geracaoPertence(){
+        if (getAnoNascimento() <= 1995) {
+            return 'geração x';
+        }
+    }
+
+    console.log('nome completo:', nomeCompleto);
+    console.log('ano de nascimento:', getAnoNascimento());
+    console.log('geração:', geracao);
+}
+
+confirmarDados('Marcos', 40);
